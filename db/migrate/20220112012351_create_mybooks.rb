8fa04b439f66e43277cb423473444522b99c1e62
EC2 Default User <ec2-user@ip-172-31-36-77.ec2.internal>
2022-01-12 02:00:54 +0000

    [Add]migrate

---
+++ db/migrate/20220112012351_create_mybooks.rb
@@ -0,0 +1,11 @@
+class CreateMybooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :mybooks do |t|
+      
+      # t.string:title
+      # t.string:body
+
+      t.timestamps
+    end
+  end
+end
